071db353f5926e23a61e6b1ecce5aceeb35c1385
ZZergling <joyjune95@gmail.com>
2021-04-07 18:56:00 +0900

    View Select

--- Cointerest/app/src/main/java/com/project/cointerest/Adapter/SearchFragmentRecyclerAdapter.kt
+++ Cointerest/app/src/main/java/com/project/cointerest/Adapter/SearchFragmentRecyclerAdapter.kt
@@ -21,6 +21,17 @@ class SearchFragmentRecyclerAdapter(val context: Context, var coin_list:ArrayLis
     val unFilteredList = coin_list // 필터 전
     var filteredList = coin_list // 필터 후
 
+    //선택한 아이템리스트
+    var selectedList = ArrayList<CoinData>()
+
+    //클릭 인터페이스 정의
+    interface ItemClickListener {
+        fun onClick(view: View, position: Int)
+    }
+
+    //클릭리스너 선언
+    private lateinit var itemClickListner: ItemClickListener
+
 
     override fun getItemCount(): Int = filteredList.size
     override fun getFilter(): Filter {
@@ -58,7 +69,7 @@ class SearchFragmentRecyclerAdapter(val context: Context, var coin_list:ArrayLis
                 println("result")
                 println(filteredList.size)
 
-                //수정이 안됨
+                //이거했는데 수정반영이 안됨
                 notifyDataSetChanged()
             }
 
@@ -95,10 +106,61 @@ class SearchFragmentRecyclerAdapter(val context: Context, var coin_list:ArrayLis
 
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: Holder, position: Int) {
+
+        //여기에는 데이터셋 수정이 적용 안됐음
         println("check@@@!!")
         println(filteredList.size)
+
         holder?.bind(filteredList[position], context)
+
+        holder.itemView.setOnClickListener {
+            var duplicateCheck = 0
+            //println("클릭 체크메이드")
+
+            println("${filteredList[position]} 선택")
+
+            var idx = 0
+
+            //원인은 삭제로직에 있는거같음(해결)
+            for(coin in selectedList){
+                if((filteredList[position].kor_name == coin.kor_name)
+                        && (filteredList[position].market == coin.market)){
+
+                    selectedList.remove(selectedList[idx])
+
+                    //println("${idx}번째 인덱스")
+                    println("${filteredList[position].kor_name} 선택해제")
+
+                    println("##선택된 코인들###")
+                    for(Ncoin in selectedList){
+                        println("${Ncoin.kor_name} - ${Ncoin.market}")
+                    }
+                    println("#################")
+
+                    duplicateCheck = 1
+                    break
+                }
+                idx++
+            }
+
+            //리스트 중간에 삭제됐던거 다시 추가하면 튕김
+            if(duplicateCheck == 0){
+                println("추가!!")
+                selectedList.add(filteredList[position])
+            }
+
+
+            println("##선택된 코인들###")
+            for(coin in selectedList){
+                println("${coin.kor_name} - ${coin.market}")
+            }
+            println("#################")
+
+
+        }
+
     }
+
 
 
 }
