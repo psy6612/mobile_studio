95318e912471f29af44bf410104637a1b668b308
psy6612 <psy12psy@naver.com>
2021-04-27 18:12:53 +0900

    setting_fragment(1)

--- Cointerest/app/src/main/java/com/project/cointerest/Fragment/settingFragment.kt
+++ Cointerest/app/src/main/java/com/project/cointerest/Fragment/settingFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.MenuItem
+import android.widget.PopupMenu
 import com.project.cointerest.R
+import kotlinx.android.synthetic.main.fragment_setting.*
 
 
 class settingFragment : Fragment() {
@@ -16,7 +19,10 @@ class settingFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_setting, container, false)
+
     }
+
+
 
 
 }
--- Cointerest/app/src/main/java/com/project/cointerest/MainActivity.kt
+++ Cointerest/app/src/main/java/com/project/cointerest/MainActivity.kt
@@ -5,6 +5,7 @@ package com.project.cointerest
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import okhttp3.*
@@ -12,9 +13,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.project.cointerest.Fragment.*
 import kotlinx.android.synthetic.main.fragment_search.*
-
+import kotlinx.android.synthetic.main.fragment_setting.*
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,12 +24,10 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
         println("GO")
         tl_ac_main_bottom_menu.setOnNavigationItemSelectedListener(this)
-        supportFragmentManager.beginTransaction().add(R.id.vp_ac_main_frag_pager, coinFragment()).commit()
-
-
-
-
+        supportFragmentManager.beginTransaction().add(R.id.vp_ac_main_frag_pager, coinFragment())
+            .commit()
     }
+
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         var coinFrag :Fragment
@@ -49,7 +49,8 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             R.id.marketItem -> {
                 supportFragmentManager.beginTransaction().replace(R.id.vp_ac_main_frag_pager, marketFragment()).commitAllowingStateLoss()
                 if(coinFrag== null){
-                    fragmentManager.beginTransaction().add(R.id.coinItem, coinFrag).commit();
+                    fragmentManager.beginTransaction().add(R.id.coinItem,
+                            coinFrag).commit();
                 }
                 return true
             }
@@ -70,5 +71,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         }
         return false
     }
+
+
 
 }
